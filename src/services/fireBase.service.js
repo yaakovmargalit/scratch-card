a0079f75acb55719dc33f0cc12fc38958efed41c
יעקב מרגלית <you@example.com>
2021-12-23 10:05:58 +0200

    first commit

---
+++ src/services/fireBase.service.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDwcDtFkLnWEBGWq-ZR3Aq-ngW_tw7tpgk",
+    authDomain: "scratch-card-8731a.firebaseapp.com",
+    projectId: "scratch-card-8731a",
+    storageBucket: "scratch-card-8731a.appspot.com",
+    messagingSenderId: "94990018567",
+    appId: "1:94990018567:web:9dd61009655f6a431da2a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
